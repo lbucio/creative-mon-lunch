43a77487143cd67bd356f0deac26c05e069dae5b
Matt Darrington <mdarrington@lucidchart.com>
2019-08-28 15:47:32 -0600

    first attempt at somewhat meaningful predictions

--- src/pages/leaderboard/leaderboard.tsx
+++ src/pages/leaderboard/leaderboard.tsx
@@ -1,60 +1,58 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
-import './leaderboard.scss';
+import React from "react";
+import { RouteComponentProps } from "@reach/router";
+import "./leaderboard.scss";
 
-interface Props extends RouteComponentProps {
-}
+interface Props extends RouteComponentProps {}
 const items = [
-    {
-        name: "Owen",
-        score: "10%",
-    },
-    {
-        name: "Owen",
-        score: "10%",
-    },
-    {
-        name: "Owen",
-        score: "10%",
-    },
-    {
-        name: "Owen",
-        score: "10%",
-    },
-    {
-        name: "Owen",
-        score: "10%",
-    },
-    {
-        name: "Owen",
-        score: "10%",
-    },
-    {
-        name: "Owen",
-        score: "10%",
-    },
-]
-const Leaderboard: React.FC<Props> = ({  }) => {
-    return (
-        <main className="leaderboard">
-            <section>
-                <h1>Leaderboard</h1>
-                <div>
-                    <h2>Current Champ</h2>
-                </div>
-                <div className="leaderboard__item">
-                    <h3>Name</h3>
-                    <p>Score</p>
-                </div>
-                {items.map(item => (
-                    <div className="leaderboard__item">
-                        <h3>{item.name}</h3>
-                        <p>{item.score}</p>
-                    </div>
-                ))}
-            </section>
-        </main>
-    );
-}
-          
+  {
+    name: "Owen",
+    score: "10%"
+  },
+  {
+    name: "Owen",
+    score: "10%"
+  },
+  {
+    name: "Owen",
+    score: "10%"
+  },
+  {
+    name: "Owen",
+    score: "10%"
+  },
+  {
+    name: "Owen",
+    score: "10%"
+  },
+  {
+    name: "Owen",
+    score: "10%"
+  },
+  {
+    name: "Owen",
+    score: "10%"
+  }
+];
+
+const Leaderboard: React.FC<Props> = () => {
+  return (
+    <section>
+      <h1>Leaderboard</h1>
+      <div>
+        <h2>Current Champ</h2>
+      </div>
+      <div className="leaderboard__item">
+        <h3>Name</h3>
+        <p>Score</p>
+      </div>
+      {items.map(item => (
+        <div className="leaderboard__item">
+          <h3>{item.name}</h3>
+          <p>{item.score}</p>
+        </div>
+      ))}
+    </section>
+  );
+};
+
 export default Leaderboard;
--- src/pages/predictions/predictions.tsx
+++ src/pages/predictions/predictions.tsx
@@ -1,14 +1,72 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { useState, useEffect } from "react";
+import { RouteComponentProps } from "@reach/router";
+import brain from "brain.js";
+
+import data from "../../data/historical";
+
+import "./predictions.scss";
+
+interface netValue {
+  [key: string]: number;
+}
 
 interface Props extends RouteComponentProps {
-    prediction?: string;
+  prediction?: string;
 }
 
+const trainingData = data.map(week => {
+  const day = parseInt(week.Date.split("/")[1]);
+  const weekNumber = Math.round(day / 7);
+  const output: netValue = {};
+  output[week.Actual.toLocaleLowerCase()] = 1;
+  return {
+    input: { weekNumber },
+    output
+  };
+});
+
+const net = new brain.NeuralNetwork();
+
+net.train(trainingData);
+
 const Predictions: React.FC<Props> = ({ prediction }) => {
-    return (
-        <main>Predictions! {prediction}</main>
+  const [weekNumber, setWeekNumber] = useState(0);
+  const [guesses, setGuesses] = useState(net.run(weekNumber));
+  const [sortedGuesses, setSortedGuesses] = useState(
+    Object.keys(guesses).sort((a, b) => guesses[b] - guesses[a])
+  );
+
+  useEffect(() => {
+    if (weekNumber >= 0) {
+      setGuesses(net.run({ weekNumber }));
+    }
+  }, [weekNumber]);
+
+  useEffect(() => {
+    setSortedGuesses(
+      Object.keys(guesses).sort((a, b) => guesses[b] - guesses[a])
     );
-}
-          
+  }, [guesses]);
+
+  return (
+    <section className="predictions">
+      <h1>Predictions</h1>
+      <input
+        type="number"
+        onChange={e => setWeekNumber(parseInt(e.target.value))}
+      />
+      <div className="predictions__item">
+        <h3>Guess</h3>
+        <p>Probability</p>
+      </div>
+      {sortedGuesses.map((guess: any) => (
+        <div key={guess} className="predictions__item">
+          <h3>{guess}</h3>
+          <h3>{(guesses[guess] * 100).toFixed(0)}%</h3>
+        </div>
+      ))}
+    </section>
+  );
+};
+
 export default Predictions;
