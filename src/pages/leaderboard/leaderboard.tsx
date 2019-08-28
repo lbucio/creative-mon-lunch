c7b7ae9213832d216438fd0dace1ed79d43a3671
Lars Bucio <lbucio@lucidchart.com>
2019-08-28 11:00:15 -0600

    feat: Scaffold leaderboard

---
+++ src/pages/leaderboard/leaderboard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { RouteComponentProps } from '@reach/router';
+import './leaderboard.scss';
+
+interface Props extends RouteComponentProps {
+}
+const items = [
+    {
+        name: "Owen",
+        score: "10%",
+    },
+    {
+        name: "Owen",
+        score: "10%",
+    },
+    {
+        name: "Owen",
+        score: "10%",
+    },
+    {
+        name: "Owen",
+        score: "10%",
+    },
+    {
+        name: "Owen",
+        score: "10%",
+    },
+    {
+        name: "Owen",
+        score: "10%",
+    },
+    {
+        name: "Owen",
+        score: "10%",
+    },
+]
+const Leaderboard: React.FC<Props> = ({  }) => {
+    return (
+        <main className="leaderboard">
+            <section>
+                <h1>Leaderboard</h1>
+                <div>
+                    <h2>Current Champ</h2>
+                </div>
+                <div className="leaderboard__item">
+                    <h3>Name</h3>
+                    <p>Score</p>
+                </div>
+                {items.map(item => (
+                    <div className="leaderboard__item">
+                        <h3>{item.name}</h3>
+                        <p>{item.score}</p>
+                    </div>
+                ))}
+            </section>
+        </main>
+    );
+}
+          
+export default Leaderboard;
