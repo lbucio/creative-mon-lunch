592b28fdcfc43e0245057797bc74009ab3c4b551
Matt Darrington <mdarrington@lucidchart.com>
2019-08-29 14:12:48 -0600

    no previous

--- src/pages/predictions/predictions.tsx
+++ src/pages/predictions/predictions.tsx
@@ -14,81 +14,63 @@ interface Props extends RouteComponentProps {
   prediction?: string;
 }
 
-// const encode = (arg: string) => {
-//   const array = arg.split("").map(x => x.charCodeAt(0) / 255);
-//   return array.reduce((value: number, current: number) => {
-//     value += current;
-//     return value / array.length;
-//   }, 0);
-// };
+const count: netValue = {};
 
 const trainingData = data.map((week: any, i) => {
-  const day = parseInt(week.Date.split("/")[1]);
-  // const weekNumber = Math.round(day / 7);
-  // const output: netValue = {};
-  let previous = "Zupas";
-  if (i > 0) {
-    previous = data[i - 1].Actual;
-  }
+  const rotationNumber = i % 6;
+  const output: netValue = {};
   const actual = week.Actual.toLocaleLowerCase();
 
-  // output = actual;
-  // const input = {  };
+  output[actual] = 1;
+  count[actual] = count[actual] ? count[actual] + 1 : 1;
+  const input = { rotationNumber };
 
   return {
-    input: previous,
-    output: actual
+    input,
+    output
   };
 });
 
-console.log(trainingData);
+const filteredData = trainingData.filter(
+  // only keep results that have happened more than twice
+  week => count[Object.keys(week.output)[0]] > 2
+);
 
-const net = new brain.recurrent.LSTM();
+const net = new brain.NeuralNetwork();
 
-// create configuration for training
-const config = {
-  iterations: 300,
-  log: true,
-  logPeriod: 50,
-  layers: [10]
-};
-
-net.train(trainingData, config);
+net.train(filteredData);
 
 const Predictions: React.FC<Props> = () => {
-  const previousWeekActual = data[data.length - 1].Actual.toLowerCase();
-  const output = net.run("zupas");
-  console.log(output);
-  // const [weekNumber, setWeekNumber] = useState(0);
-  // const [guesses, setGuesses] = useState(net.run(previousWeekActual));
-  // const [sortedGuesses, setSortedGuesses] = useState(
-  //   Object.keys(guesses).sort((a, b) => guesses[b] - guesses[a])
-  // );
+  const [rotationNumber, setRotationNumber] = useState(data.length % 6);
+  const [guesses, setGuesses] = useState(net.run({ rotationNumber }));
+  const [sortedGuesses, setSortedGuesses] = useState(
+    Object.keys(guesses).sort((a, b) => guesses[b] - guesses[a])
+  );
 
-  // useEffect(() => {
-  //   if (weekNumber >= 0) {
-  //     setGuesses(
-  //       net.run({
-  //         weekNumber: weekNumber
-  //       })
-  //     );
-  //   }
-  // }, [weekNumber]);
+  useEffect(() => {
+    if (rotationNumber >= 0) {
+      setGuesses(
+        net.run({
+          rotationNumber
+        })
+      );
+    }
+  }, [rotationNumber]);
 
-  // useEffect(() => {
-  //   setSortedGuesses(
-  //     Object.keys(guesses).sort((a, b) => guesses[b] - guesses[a])
-  //   );
-  // }, [guesses]);
+  useEffect(() => {
+    setSortedGuesses(
+      Object.keys(guesses).sort((a, b) => guesses[b] - guesses[a])
+    );
+  }, [guesses]);
 
   return (
     <section className="predictions">
       <h1>Predictions</h1>
-      {/* <input
-        className="predictions__input"
+      <input
+        className="predictions__input-number"
         type="number"
-        onChange={e => setWeekNumber(parseInt(e.target.value))}
-        value={weekNumber}
+        onChange={e => setRotationNumber(parseInt(e.target.value))}
+        value={rotationNumber}
         min={0}
         max={5}
       />
@@ -101,7 +83,7 @@ const Predictions: React.FC<Props> = () => {
           <h3>{guess}</h3>
           <h3>{(guesses[guess] * 100).toFixed(0)}%</h3>
         </div>
-      ))} */}
+      ))}
     </section>
   );
 };
