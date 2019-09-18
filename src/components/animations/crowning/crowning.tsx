4704d8f318c55a51a219a9187920dbe731159326
Emilie Martinez <emilieknecht@lucidchart.com>
2019-09-18 10:40:46 -0600

    crowning

--- src/components/animations/crowning/crowning.tsx
+++ src/components/animations/crowning/crowning.tsx
@@ -1,30 +1,109 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { RouteComponentProps } from "@reach/router";
+// import * as easings from 'd3-ease';
 
-import { useSpring, animated } from 'react-spring'
+import { useSpring, animated, useTransition } from "react-spring";
 
 import "./crowning.scss";
-import crown from "./crown.svg";
+import medal from "./crown.svg";
 
-import emilie from "../../../assets/people/emilie-knecht.png"
+import emilie from "../../../assets/people/emilie-knecht.png";
 
-interface Props extends RouteComponentProps {}
-
-function Crowning() {
-  const { top, opacity } = useSpring({
-    opacity: 1,
-    top: `-35px`
-  })
-  
-  return <animated.img src={crown} className="crown" style={{ opacity, top }} />
+interface Props {
+  clicked: boolean;
 }
 
-const CrowningCeremony: React.FC<Props> = () => {
+const CrowningCeremony = (clicked: Props) => {
+  const fade = useTransition(clicked, null, {
+    from: { position: "relative", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
+  const podiumTransitionThird = useSpring({
+    to: { size: '25px', background: 'red' },
+    from: { size: '0%', background: 'white' }
+  })
+
+  const podiumTransitionFirst = useSpring({
+    to: { size: '75px', background: 'red' },
+    from: { size: '0%', background: 'white' }
+  })
+
+  const podiumTransitionSecond = useSpring({
+    to: { size: '50px', background: 'red' },
+    from: { size: '0%', background: 'white' }
+  })
 
   return (
-    <div className="svg-animation__container">
-      <Crowning />
-      <img src = {emilie} className="people" />
+    <div className="podium">
+      <div className="third-place">
+        {fade.map(
+          ({ item, key, props }) =>
+            item && (
+              <>
+                <animated.img
+                  src={medal}
+                  key={key}
+                  style={props}
+                  className="medal"
+                />
+                <animated.img
+                  key={key}
+                  style={props}
+                  src={emilie}
+                  className="people"
+                />
+              </>
+            )
+        )}
+
+      <animated.div style={podiumTransitionThird} className="third-podium">3</animated.div>
+      </div>
+      <div className="first-place">
+        {fade.map(
+          ({ item, key, props }) =>
+            item && (
+              <>
+                <animated.img
+                  src={medal}
+                  key={key}
+                  style={props}
+                  className="medal"
+                />
+                <animated.img
+                  key={key}
+                  style={props}
+                  src={emilie}
+                  className="people"
+                />
+              </>
+            )
+        )}
+        <animated.div style={podiumTransitionFirst} className="first-podium">1</animated.div>
+      </div>
+      <div className="second-place">
+        {fade.map(
+          ({ item, key, props }) =>
+            item && (
+              <>
+                <animated.img
+                  src={medal}
+                  key={key}
+                  style={props}
+                  className="medal"
+                />
+                <animated.img
+                  key={key}
+                  style={props}
+                  src={emilie}
+                  className="people"
+                />
+              </>
+            )
+        )}
+        <animated.div style={podiumTransitionSecond} className="second-podium">2</animated.div>
+      </div>
     </div>
   );
 };
--- src/pages/leaderboard/leaderboard.tsx
+++ src/pages/leaderboard/leaderboard.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RouteComponentProps } from "@reach/router";
 import "./leaderboard.scss";
 
-import Crowning from '../../components/animations/crowning/crowning';
+import Crowning from "../../components/animations/crowning/crowning";
 
 interface Props extends RouteComponentProps {}
 const items = [
@@ -37,23 +37,36 @@ const items = [
 ];
 
 const Leaderboard: React.FC<Props> = () => {
+  const [clickedButton, setClickedButton] = useState(false);
+
+  useEffect(() => {
+  }, [clickedButton]);
+
   return (
     <section className="container">
       <h1>Leaderboard</h1>
-      <div>
-        <Crowning />
-        <h2>Current Champ</h2>
-      </div>
-      <div className="leaderboard__item">
-        <h3>Name</h3>
-        <p>Score</p>
-      </div>
-      {items.map(item => (
-        <div className="leaderboard__item">
-          <h3>{item.name}</h3>
-          <p>{item.score}</p>
+
+      {clickedButton && (
+        <div>
+          <div>
+            <Crowning clicked={clickedButton}/>
+            <h2>Current Champ</h2>
+          </div>
+          <div className="leaderboard__item">
+            <h3>Name</h3>
+            <p>Score</p>
+          </div>
+          {items.map(item => (
+            <div className="leaderboard__item">
+              <h3>{item.name}</h3>
+              <p>{item.score}</p>
+            </div>
+          ))}{" "}
         </div>
-      ))}
+      )}
+      <button onClick={() => setClickedButton(a => !a)} className="button">
+        Who's the winner?
+      </button>
     </section>
   );
 };
